ff7c919ae398e4a45b2855c5c0d1da837c6fc08e
MaviyaQureshi <maviyaqureshi2003@gmail.com>
2023-12-15 15:50:22 +0530

    responsive..

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,12 +4,12 @@ const Footer = () => {
     return (
         <>
             <div className='bg-primary flex flex-col justify-center items-center'>
-                <div className='mt-4 md:mt-3 text-base md:text-lg space-x-10'>
+                <div className='mt-4 sm:mt-3 text-sm sm:text-base md:text-lg space-x-10'>
                     <a href="https://lyfeindex.com/Terms_of_use/"><button>Terms</button></a>
                     <a href="https://lyfeindex.com/Contact_us/"><button>Privacy Policy </button></a>
                 </div>
 
-                <p className='text-sm mt-3'>LyfeIndex © 2023</p>
+                <p className='text-xs sm:text-sm mt-3'>LyfeIndex © 2023</p>
             </div>
         </>
     )
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,17 +3,17 @@ import React from 'react'
 const Navbar = () => {
     return (
         <>
-            <div className='px-16 md:px-28 flex justify-between items-center h-24 bg-primary'>
+            <div className='px-5 sm:px-16 md:px-28 flex justify-between items-center h-24 bg-primary'>
                 <div className='flex flex-col justify-center items-center'>
-                    <span className='sm:text-xl md:text-2xl'>
-                        <span className='text-2xl md:text-3xl'>L</span>YFE
-                        <span className='text-2xl md:text-3xl'>I</span>NDEX
+                    <span className='text-base sm:text-xl md:text-2xl'>
+                        <span className='text-xl sm:text-2xl md:text-3xl'>L</span>YFE
+                        <span className='text-xl sm:text-2xl md:text-3xl'>I</span>NDEX
                     </span>
-                    <span className='text-xs'>
+                    <span className='text-[9px] sm:text-xs'>
                         Love You 4 Ever
                     </span>
                 </div>
-                <span className='flex justify-between w-56 text-sm md:w-80 md:text-lg font-medium'>
+                <span className='flex justify-between w-44 sm:w-56 text-xs sm:text-sm md:w-80 md:text-lg font-medium'>
                     <a href="#"><button className='hover:text-xl ease-in duration-100'>HOME <hr className='w-6 rounded-full ease-in duration-100' /></button></a>
                     <a href="https://lyfeindex.com/Contact_us/"><button className='hover:text-xl ease-in duration-100'>CONTACT <hr className='w-6 rounded-full' /> </button></a>
                     <a href="https://lyfeindex.com/About/"><button className='hover:text-xl ease-in duration-100'>ABOUT US <hr className='w-6 rounded-full' /> </button></a>
