d06e36daa56c356845def51ba50cca18a7000851
MaviyaQureshi <maviyaqureshi2003@gmail.com>
2023-12-15 16:11:16 +0530

    full responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
             <div className='bg-primary flex flex-col justify-center items-center'>
                 <div className='mt-4 sm:mt-3 text-sm sm:text-base md:text-lg space-x-10'>
                     <a href="https://lyfeindex.com/Terms_of_use/"><button>Terms</button></a>
-                    <a href="https://lyfeindex.com/Contact_us/"><button>Privacy Policy </button></a>
+                    <a href="https://lyfeindex.com/Privacy_policy/"><button>Privacy Policy </button></a>
                 </div>
 
                 <p className='text-xs sm:text-sm mt-3'>LyfeIndex © 2023</p>
